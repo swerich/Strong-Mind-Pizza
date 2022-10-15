22d25f89f7bfbb129c504075f2b469f9cc431ef7
swerich <richard.scott.swe@gmail.com>
2022-10-15 10:51:08 -0700

    Chef & User Permissions working.

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(max_length=50)
+    owner = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Chef(models.Model):
     name = models.CharField(max_length=50)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
 class Pizza(models.Model):
     name = models.CharField(max_length=50, unique=True)
--- pizzas/views.py
+++ pizzas/views.py
@@ -2,45 +2,93 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from pizzas.models import Pizza, Topping
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
+
+
+
+
+# Authorization
+
+def is_chef(user):
+    try:
+        user.chef
+        return True 
+    except Exception:
+        return False
+    
+def is_owner(user):
+    try:
+        user.store
+        return True
+    except Exception:
+        return False
+    
+    
 # Create your views here.
-class PizzaListView(ListView):
+class PizzaListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Pizza 
     template_name = "pizzas/pizza_list.html"
     
-class CreatePizzaView(CreateView):
+    def test_func(self):
+        return is_chef(self.request.user)
+    
+    
+    
+class CreatePizzaView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Pizza 
     template_name = "pizzas/create_pizza.html"
     fields = ["name", "toppings"]
     success_url = '/pizzas/'
     
-class UpdatePizzaView(UpdateView):
+    def test_func(self):
+        return is_chef(self.request.user)
+    
+class UpdatePizzaView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Pizza 
     template_name = "pizzas/update_pizza.html"
     fields = ["name", "toppings"]
     success_url = '/pizzas/'
     
-class DeletePizzaView(DeleteView):
+    def test_func(self):
+        return is_chef(self.request.user)
+    
+class DeletePizzaView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Pizza
     success_url = '/pizzas/'
     
-class ToppingListView(ListView):
+    def test_func(self):
+        return is_chef(self.request.user)
+    
+class ToppingListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Topping
     template_name = "pizzas/toppings_list.html"
     
-class CreateToppingView(CreateView):
+    def test_func(self):
+        return is_owner(self.request.user)  
+    
+class CreateToppingView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Topping 
     template_name = "pizzas/create_topping.html"
     fields = ["name"]
     success_url = '/pizzas/toppings/'
     
-class UpdateToppingView(UpdateView):
+    def test_func(self):
+        return is_owner(self.request.user)  
+    
+class UpdateToppingView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Topping 
     template_name = "pizzas/update_topping.html"
     fields = ["name"]
     success_url = '/pizzas/toppings/'
     
-class DeleteToppingView(DeleteView):
+    def test_func(self):
+        return is_owner(self.request.user)  
+    
+class DeleteToppingView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Topping
     success_url = '/pizzas/toppings/'
+    
+    def test_func(self):
+        return is_owner(self.request.user)  
