158f7f585c19415c14306d54e1d1527dc9f4e5ff
swerich <richard.scott.swe@gmail.com>
2022-10-15 14:33:16 -0700

    Changed STATIC_ROOT to equal static.

--- smp/settings.py
+++ smp/settings.py
@@ -131,6 +131,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = 'static'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
